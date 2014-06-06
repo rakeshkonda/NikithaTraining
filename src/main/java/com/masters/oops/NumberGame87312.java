b528fa45e0feba640ca974f78c980869ee0b98c6
Rakesh Konda <rakesh.konda@am.sony.com>
2014-06-05 21:20:28 -0700

    code fix

--- src/main/java/com/masters/oops/NumberGame87312.java
+++ src/main/java/com/masters/oops/NumberGame87312.java
@@ -2,7 +2,6 @@ package com.masters.oops;
 
 import java.util.Random;
 import java.util.Scanner;
-import static java.lang.System.exit;
 
 /**
  * @author nikithaperumalla
@@ -10,53 +9,64 @@ import static java.lang.System.exit;
 
  public class NumberGame87312{
     public static void main(String[] args) {
-        int totalNumberOfGames = 0;
-        int totalNumberOfWins = 0;
-        int totalNumberOfLoses = 0;
+        playRandomNumberGame();
+    }
 
-        int userSelectedNumber = 0;
+    private static void playRandomNumberGame() {
         Scanner sc = new Scanner(System.in);
-        Random random = new Random();
-        int randomNum = random.nextInt(10);
 
-        while(randomNum != userSelectedNumber){
-                //for loop to control the 3 trials
-                for(int j = 1 ; j <= 3; j++) {
-                    System.out.println("Enter a number >= 1 and <= 10, or enter -1 to exit game:");
-                    userSelectedNumber = sc.nextInt();
+        try {
+            int totalNumberOfGames = 0;
+            int totalNumberOfWins = 0;
+            int totalNumberOfLoses = 0;
 
-                    //user chooses to exit the game
-                    if (userSelectedNumber == -1) {
-                        System.out.println("You chose to exit the game. Exiting the game");
-                        System.out.println("Game Summary");
-                        System.out.println("Total Games: "+totalNumberOfGames);
-                        System.out.println("Number of Wins: "+totalNumberOfWins);
-                        System.out.println("Number of Losts: "+totalNumberOfLoses);
-                        exit(1);
-                    }
+            int userSelectedNumber = 0;
+            Random random = new Random();
+            int randomNum = random.nextInt(10);
 
-                    //wins the game
-                    if (userSelectedNumber == randomNum) {
-                        totalNumberOfGames++;
-                        totalNumberOfWins++;  //increasing the win count
-                        System.out.println("Congratulations." + "Your guess is correct. \n"
-                                + "A new game starts now. \n");
-                        randomNum = random.nextInt(10);
-                        break;
-                    } else {
-                        totalNumberOfGames++;
-                        System.out.println("Your guess is incorrect. Try again");
-                        // looses the game
-                        totalNumberOfLoses++; // increasing the losts count
-                    }
-                    if(j == 3)  {
-                        System.out.println("3 trials have been exceeded." +
-                                " The correct answer is "+randomNum);
+            while(randomNum != userSelectedNumber){
+                    //for loop to control the 3 trials
+                    for(int j = 1 ; j <= 3; j++) {
+                        System.out.println("Enter a number >= 1 and <= 10, or enter -1 to exit game:");
+                        userSelectedNumber = sc.nextInt();
 
-                        System.out.println("A new game starts now");
-                        randomNum = random.nextInt(10);
+                        //user chooses to exit the game
+                        if (userSelectedNumber == -1) {
+                            System.out.println("You chose to exit the game. Exiting the game");
+                            System.out.println("Game Summary");
+                            System.out.println("Total Games: "+totalNumberOfGames);
+                            System.out.println("Number of Wins: "+totalNumberOfWins);
+                            System.out.println("Number of Losts: "+totalNumberOfLoses);
+                            return;
+                        }
+
+                        //wins the game
+                        if (userSelectedNumber == randomNum) {
+                            totalNumberOfGames++;
+                            totalNumberOfWins++;  //increasing the win count
+                            System.out.println("Congratulations." + "Your guess is correct. \n"
+                                    + "A new game starts now. \n");
+                            randomNum = random.nextInt(10);
+                            break;
+                        } else {
+                            totalNumberOfGames++;
+                            System.out.println("Your guess is incorrect. Try again");
+                            // looses the game
+                            totalNumberOfLoses++; // increasing the losts count
+                        }
+                        if(j == 3)  {
+                            System.out.println("3 trials have been exceeded." +
+                                    " The correct answer is "+randomNum);
+
+                            System.out.println("A new game starts now");
+                            randomNum = random.nextInt(10);
+                        }
                     }
                 }
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }  finally {
+            sc.close();
         }
     }
 }
