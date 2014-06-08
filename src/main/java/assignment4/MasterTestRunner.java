1d3b82377e1437703ac6ed28042074b3023125f3
Rakesh Konda <rakesh.konda@am.sony.com>
2014-06-08 02:44:26 -0700

    Master TestRunner added..Assignment 4

---
+++ src/main/java/assignment4/MasterTestRunner.java
@@ -0,0 +1,26 @@
+package assignment4;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+/**
+ * @author nikithaperumalla
+ * @id 87312
+ * This the master test runner that builds the test suite
+ * and runs the tests that are added to the test suite
+ */
+public class MasterTestRunner extends TestCase {
+
+    public static void main(String[] args) {
+        //Test runner to run the suite of tests
+        TestRunner.run(suite());
+    }
+
+    public static Test suite() {
+        TestSuite result = new TestSuite();
+        result.addTest(new TestSuite(CustomerTest.class));
+        return result;
+    }
+}
